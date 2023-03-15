bdc6fabdb85885f22914aa7248a74606ac4bb6d1
lucasew <lucas59356@gmail.com>
2023-03-15 17:09:28 -0300

    multi source support
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- package.nix
+++ package.nix
@@ -1,4 +1,9 @@
-{ requireFile, fetchurl, stdenvNoCC, nix }:
+{ requireFile
+, fetchurl
+, stdenvNoCC
+, nix
+, requireFileSources ? [ ./data ]
+}:
 { name ? null
 , sha256 ? null
 , sha1 ? null
@@ -15,31 +20,34 @@ let
   hasHash = sha256 != null || sha1 != null;
   hasUrls = length urls > 0;
 
+  drvName = if name == null then baseNameOf (toString url) else name;
   hashAlgo = if sha256 != null then "sha256" else "sha1";
   hash = if sha256 != null then sha256 else sha1;
 
   urlsDrv = stdenvNoCC.mkDerivation {
-    name = "urls-${hashAlgo}-${hash}";
     allowSubstitutes = false;
     preferLocalBuild = true;
+    name = "urls-${hashAlgo}-${hash}";
 
     nativeBuildInputs = [ nix ];
 
-    src = ./data;
+    srcs = requireFileSources;
 
     installPhase = ''
       hash="$(nix --extra-experimental-features nix-command hash to-base16 --type ${hashAlgo} "${hash}")"
       {
-        cat "$src/data/http/${hashAlgo}/$hash.txt" || true
+        for src in $srcs; do
+          cat "$src/http/${hashAlgo}/$hash.txt" || true
+        done
       } > $out
     '';
   };
   urls = filter (item: isString item && item != "") (split "\n" (readFile urlsDrv));
 
   item = fetchurl {
-    name = if name == null then baseNameOf (toString url) else name;
-    inherit (args) sha256 sha1;
+    name = drvName;
     inherit urls;
+    inherit (args) sha256 sha1;
     preferLocalBuild = true;
     passthru = {
       inherit urlsDrv urls;
@@ -49,4 +57,4 @@ let
 in if !(isFlat && hasHash && hasUrls) then
   originalRequireFile
 else
-  item
+  builtins.trace "nix-requirefile: found urls for ${drvName}" item
